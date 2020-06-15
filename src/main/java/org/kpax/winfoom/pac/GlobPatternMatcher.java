5c4b3c0f178e2d9779803593fd704514d7be4a90
ecovaci <Qsim#ai?1>
2020-06-15 12:08:11 +0300

    Glob revisited

--- src/main/java/org/kpax/winfoom/pac/GlobPatternMatcher.java
+++ src/main/java/org/kpax/winfoom/pac/GlobPatternMatcher.java
@@ -17,6 +17,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import java.util.regex.Pattern;
 
@@ -32,22 +33,16 @@ public class GlobPatternMatcher {
 
     /**
      * Translate a GLOB pattern into a {@link Pattern} instance.
-     * <p>
-     * This method supports all GLOB wildcards, such as
-     * <table>
-     * <tr align="left"><td>{@code *}</td><td>matches any number of any
-     * characters including none</td>
-     * <tr align="left"><td>{@code ?}</td><td>matches any single character</td>
-     * <tr align="left"><td>{@code [abc]}</td><td>matches one character given in
-     * the bracket</td>
-     * <tr align="left"><td>{@code [a-z]}</td><td>matches one character from the
-     * range given in the bracket</td>
-     * <tr align="left"><td>{@code [!abc]}</td><td>matches one character
-     * <i>not</i> given in the bracket</td>
-     * <tr align="left"><td>{@code [!a-z]}</td><td>matches one character
-     * <i>not</i> from the range given in the bracket</td>
-     * </table>
-     *
+     * <ul>
+     * <li> The {@code *} character matches zero or more characters.</li>
+     * <li> {@code ?} matches exactly one character.</li>
+     * <li> {@code {ab,cd,ef} }matches exactly one of the "parts" provided.
+     *  These "parts" can also span multiple "sections" of the glob, so you can include directory separators in them.
+     * For example, {@code {ab,cd/ef}/*.js} is valid and would match ab/file.js as well as {@code cd/ef/file.js}.</li>
+     * <li>  The use of {@code [abc]} matches a range of characters.
+     *  If the first character of the range is {@code !} or {@code ^} then it matches any character not in the range.</li>
+     * <li> When {@code **}, the "globstar", is used it matches zero or more directories and subdirectories. This allows for recursive directory searching easily.</li>
+     *</ul>
      * <p>
      * <b>Note:</b> The result is cached.
      *
@@ -56,37 +51,45 @@ public class GlobPatternMatcher {
      */
     @Cacheable("precompiledGlobPattern")
     public Pattern toPattern(String glob) {
+        Assert.notNull(glob, "glob cannot be null");
         logger.debug("Create Pattern for {}", glob);
-        StringBuilder out = new StringBuilder();
-        out.append("^");
-        for (int i = 0; i < glob.length(); ++i) {
-            final char c = glob.charAt(i);
+        String regexPattern = convertGlobToRegEx(glob.trim());
+        logger.debug("glob regexPattern={}", regexPattern);
+        return Pattern.compile(regexPattern);
+    }
+
+    public String convertGlobToRegEx(String globExpression) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("^");
+        for (int i = 0; i < globExpression.length(); ++i) {
+            char c = globExpression.charAt(i);
             switch (c) {
                 case '*':
-                    out.append(".*?");
+                    stringBuilder.append(".*?");
                     break;
                 case '?':
-                    out.append(".{1}");
+                    stringBuilder.append(".{1}");
                     break;
                 case '.':
-                    out.append("\\.");
+                    stringBuilder.append("\\.");
                     break;
                 case '\\':
-                    out.append("\\\\");
+                    stringBuilder.append("\\\\");
                     break;
                 case '!':
-                    if (i > 0 && glob.charAt(i - 1) == '[') {
-                        out.append('^');
+                    if (i > 0 && globExpression.charAt(i - 1) == '[') {
+                        stringBuilder.append('^');
                     } else {
-                        out.append(c);
+                        stringBuilder.append(c);
                     }
                     break;
                 default:
-                    out.append(c);
+                    stringBuilder.append(c);
             }
         }
-        out.append("$");
-        return Pattern.compile(out.toString());
+        stringBuilder.append("$");
+        return stringBuilder.toString();
     }
+
 
 }
---
+++ src/test/java/org/kpax/winfoom/pac/GlobPatternMatcherTests.java
@@ -0,0 +1,83 @@
+/*
+ *  Copyright (c) 2020. Eugen Covaci
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package org.kpax.winfoom.pac;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+public class GlobPatternMatcherTests {
+
+    GlobPatternMatcher globPatternMatcher = new GlobPatternMatcher();
+
+    @Test
+    void convertGlobToRegEx_StartWithStar_Matches () {
+        Pattern pattern = Pattern.compile(globPatternMatcher.convertGlobToRegEx("*.java"));
+        boolean matches = pattern.matcher("bla.java").matches();
+        Assertions.assertTrue(matches);
+    }
+
+    @Test
+    void convertGlobToRegEx_MatchesOneCharacter_Matches () {
+        Pattern pattern = Pattern.compile(globPatternMatcher.convertGlobToRegEx("x?.java"));
+        boolean matches = pattern.matcher("xc.java").matches();
+        Assertions.assertTrue(matches);
+    }
+
+    @Test
+    void convertGlobToRegEx_MatchesOneCharacter_NotMatches () {
+        Pattern pattern = Pattern.compile(globPatternMatcher.convertGlobToRegEx("x?.java"));
+        boolean matches = pattern.matcher("xab.java").matches();
+        Assertions.assertFalse(matches);
+    }
+
+    @Test
+    void convertGlobToRegEx_Range_Matches () {
+        Pattern pattern = Pattern.compile(globPatternMatcher.convertGlobToRegEx("x[abc].java"));
+        boolean matches = pattern.matcher("xb.java").matches();
+        Assertions.assertTrue(matches);
+    }
+
+    @Test
+    void convertGlobToRegEx_Range_NotMatches () {
+        Pattern pattern = Pattern.compile(globPatternMatcher.convertGlobToRegEx("x[abc].java"));
+        boolean matches = pattern.matcher("xbc.java").matches();
+        Assertions.assertFalse(matches);
+    }
+
+    @Test
+    void convertGlobToRegEx_Group_Matches () {
+        String regex = globPatternMatcher.convertGlobToRegEx("xyz(ab|cd|ef).java");
+        System.out.println(regex);
+        Pattern pattern = Pattern.compile(regex);
+        boolean matches = pattern.matcher("xyzab.java").matches();
+        Assertions.assertTrue(matches);
+    }
+
+    @Test
+    void convertGlobToRegEx_Group_NotMatches () {
+        Pattern pattern = Pattern.compile(globPatternMatcher.convertGlobToRegEx("x(ab|cd|ef).java"));
+        boolean matches = pattern.matcher("xbc.java").matches();
+        Assertions.assertFalse(matches);
+    }
+
+    @Test
+    void convertGlobToRegEx_Globstar_Matches () {
+        Pattern pattern = Pattern.compile(globPatternMatcher.convertGlobToRegEx("**.java"));
+        boolean matches = pattern.matcher("xbc/abc/bla/foo.java").matches();
+        Assertions.assertTrue(matches);
+    }
+
+
+}
